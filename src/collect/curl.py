885677d634f3f461c2b71e45799b17931a302e36
CalebeCavalcante <calebesantos.cavalcante@gmail.com>
2020-03-19 12:45:12 -0300

    Stocks

--- src/collect/curl.py
+++ src/collect/curl.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 class Curl:
     
@@ -8,6 +9,19 @@ class Curl:
           
             response = requests.get(url, headers=headers, params=params)
              
+            if response.status_code > 308 : raise Exception("Falha ao buscar os dados. StatusCode: "+ response.status_code)
+
+            return response
+            
+        except Exception as e:
+            raise e
+    
+    def post(self, url, headers, params):
+
+        try:
+            # Exemplo: params = {'some': 'data'}
+            response = requests.post(url, data=json.dumps(params), headers=headers)
+            
             if response.status_code > 308 : raise Exception("Falha ao buscar os dados. StatusCode: "+ response.status_code)
 
             return response
